91e189ecee1c8b9e3b1911f69946d73ebcece9ae
Anabelheh <103428102+Anabelheh@users.noreply.github.com>
2022-07-31 18:30:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,1 +1,28 @@
+<?php
 
+class Conexao {
+
+	private $host = 'Servidor: sql211.epizy.com';
+	private $dbname = 'epiz_32260464_voluntario_db';
+	private $user = 'epiz_32260464';
+	private $pass = 'ARvIkLNs72S4lA';
+
+	public function conectar() {
+		try{
+
+			$conexao = new PDO(
+				"mysql:host-$this->host;dbname-$this->dbname",
+				"$this->user",
+				"$this->pass"	
+			);
+
+			return $conexao;
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessege().'</p>';
+		}
+	}
+
+}
+
+?>
